fe6c5033606927f54474e8172c012643700c0473
Joaquín Frías <joaquin124507@gmail.com>
2025-08-23 11:46:30 -0300

    Creación repositorio

---
+++ AutomationExcerciseSelenium/src/test/java/runner/TestRunner.java
@@ -0,0 +1,25 @@
+package runner;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import io.cucumber.java.After;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import pages.BasePage;
+
+    
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(features = "src/test/resources/features", // Directorio de nuestros archivos .feature
+                     glue = "steps", // Paquete donde están los steps
+                     plugin = {"pretty", "html:target/cucumber-reports.html"})
+
+                     
+public class TestRunner {
+@AfterClass
+public static void cleanDriver() {
+    // Cierra el navegador después de ejecutar todas las pruebas
+        BasePage.closeBrowser();
+        }
+}
